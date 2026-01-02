edbe21254e434df2d28f26551e41540a92e4fbb0
ctsfc9 <112675427@qq.com>
2026-01-02 22:14:38 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+// 引入 Vant 的全局样式 (有些组件需要全局样式)
+import 'vant/lib/index.css';
+
+createApp(App).mount('#app');
