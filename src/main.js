56089a8f769902a40144e71c11fffe3e186194e0
ctsfc9 <112675427@qq.com>
2026-01-08 19:22:05 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// 1. 引入 Vant 样式 (这行非常重要，否则样式会乱)
+// 1. 必须引入 Vant 的样式文件，否则组件不显示或白屏
 import 'vant/lib/index.css'
 
-// 2. 创建应用
 const app = createApp(App)
-
-// 3. 挂载
 app.mount('#app')
