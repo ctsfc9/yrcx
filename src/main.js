8492b6c662679556520e7a00e643ecc1ead1c97c
ctsfc9 <112675427@qq.com>
2026-01-10 09:54:53 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-// 1. 引入 Vant 全量组件
+// 1. 引入 Vant 组件库
 import Vant from 'vant';
-// 2. ★★★ 必须引入样式，否则白屏或布局错乱 ★★★
+// 2. ★★★ 必须引入 Vant 样式，否则白屏 ★★★
 import 'vant/lib/index.css';
 
 const app = createApp(App);
-
-// 3. 注册 Vant
 app.use(Vant);
-
-// 4. 挂载
 app.mount('#app');
