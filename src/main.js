cda7a3a31b85070e7b02fd9eddee287b7c4a3630
ctsfc9 <112675427@qq.com>
2026-05-15 15:57:58 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import App from './App.vue';
 import router from './router';
-import Vant from 'vant';
+import App from './App.vue';
+import { ConfigProvider } from 'vant';
 import 'vant/lib/index.css';
 
 const app = createApp(App);
-const pinia = createPinia();
 
-app.use(pinia);
+app.use(createPinia());
 app.use(router);
-app.use(Vant);
+app.use(ConfigProvider);
 
 app.mount('#app');
