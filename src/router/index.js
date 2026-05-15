e78e221ac4cba462fc1654d2633c2715c5b15225
ctsfc9 <112675427@qq.com>
2026-05-15 22:18:59 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,28 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/publish', component: () => import('../views/Publish.vue') },
-  { path: '/me', component: () => import('../views/Me.vue') },
-  { path: '/admin', component: () => import('../views/Admin.vue') } // 管理后台路由
+  { 
+    path: '/', 
+    component: Home 
+  },
+  { 
+    path: '/publish', 
+    component: () => import('../views/Publish.vue') 
+  },
+  { 
+    path: '/me', 
+    component: () => import('../views/Me.vue') 
+  },
+  { 
+    // 👉 修复：补回详情页路由！这样首页点击就能跳过去了
+    path: '/detail', 
+    component: () => import('../views/Detail.vue') 
+  },
+  { 
+    // 👉 确保 admin 路由注册正确
+    path: '/admin', 
+    component: () => import('../views/Admin.vue') 
+  }
 ]
 
 const router = createRouter({
