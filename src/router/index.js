fb3d9e1c7a3e93ac807070a8f7768cfff5b564c6
ctsfc9 <112675427@qq.com>
2026-05-15 23:29:42 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,33 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
-  { 
-    path: '/', 
-    component: Home 
-  },
-  { 
-    path: '/publish', 
-    component: () => import('../views/Publish.vue') 
-  },
-  { 
-    path: '/me', 
-    component: () => import('../views/Me.vue') 
-  },
-  { 
-    // 👉 修复：补回详情页路由！这样首页点击就能跳过去了
-    path: '/detail', 
-    component: () => import('../views/Detail.vue') 
-  },
-  { 
-    // 👉 确保 admin 路由注册正确
-    path: '/admin', 
-    component: () => import('../views/Admin.vue') 
-  }
+  { path: '/', component: Home },
+  { path: '/detail', component: () => import('../views/Detail.vue') },
+  { path: '/publish', component: () => import('../views/Publish.vue') },
+  { path: '/me', component: () => import('../views/Me.vue') },
+  { path: '/admin', component: () => import('../views/Admin.vue') }
 ]
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
-
-export default router
+export default createRouter({ history: createWebHistory(), routes })
