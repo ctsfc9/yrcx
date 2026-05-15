18288fa385273c0037869e85a23b2b1df580dc1c
ctsfc9 <112675427@qq.com>
2026-05-15 22:03:42 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: () => import('../views/Home.vue'), meta: { title: '首页' } },
-  { path: '/publish', name: 'Publish', component: () => import('../views/Publish.vue'), meta: { title: '发布行程' } },
-  { path: '/me', name: 'Me', component: () => import('../views/Me.vue'), meta: { title: '个人中心' } },
-  { path: '/ride/:id', name: 'Detail', component: () => import('../views/Detail.vue'), meta: { title: '行程详情' } }
-];
+  { path: '/', component: Home },
+  { path: '/publish', component: () => import('../views/Publish.vue') },
+  { path: '/me', component: () => import('../views/Me.vue') },
+  { path: '/admin', component: () => import('../views/Admin.vue') } // 管理后台路由
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
-router.beforeEach((to, from, next) => {
-  if (to.meta.title) document.title = `${to.meta.title} - 宜人出行`;
-  next();
-});
-
-export default router;
+export default router
