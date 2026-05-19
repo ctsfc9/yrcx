56e08f2fafdfde8b481bc9a2bb0551a83d46fb26
ctsfc9 <112675427@qq.com>
2026-05-19 10:34:20 +0800

    Update TabBar.vue

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -1,23 +1,10 @@
 <script setup>
-import { ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
-
-const route = useRoute();
-const active = ref(route.path);
-
-watch(() => route.path, (newPath) => {
-  active.value = newPath;
-});
 </script>
 
 <template>
-  <van-tabbar v-model="active" route placeholder safe-area-inset-bottom :z-index="999" active-color="#ff6600">
-    <van-tabbar-item replace to="/" icon="wap-home-o">首页大厅</van-tabbar-item>
-    <van-tabbar-item replace to="/publish" icon="add-circle-o">发布行程</van-tabbar-item>
-    <van-tabbar-item replace to="/user" icon="manager-o">个人中心</van-tabbar-item>
+  <van-tabbar route fixed placeholder safe-area-inset-bottom style="z-index: 9999;" active-color="#ff6600">
+    <van-tabbar-item replace to="/" icon="wap-home-o">首页</van-tabbar-item>
+    <van-tabbar-item replace to="/publish" icon="add-circle-o">发布</van-tabbar-item>
+    <van-tabbar-item replace to="/user" icon="manager-o">我的</van-tabbar-item>
   </van-tabbar>
 </template>
-
-<style scoped>
-.van-tabbar { box-shadow: 0 -2px 10px rgba(0,0,0,0.05); }
-</style>
