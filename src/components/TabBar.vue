dc3750f7dd97a88b346d22ed5c0f5e7c644eb968
ctsfc9 <112675427@qq.com>
2026-05-19 09:48:04 +0800

    Update TabBar.vue

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -1,25 +1,16 @@
 <script setup>
-import { ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
-
-const route = useRoute();
-const active = ref(route.path);
-
-watch(() => route.path, (newPath) => {
-  active.value = newPath;
-});
+// 去除复杂的监听拦截，直接依托 Vue Router
 </script>
 
 <template>
-  <van-tabbar v-model="active" route placeholder safe-area-inset-bottom :z-index="999" active-color="#ff6600">
-    <van-tabbar-item replace to="/" icon="wap-home-o" name="/">首页大厅</van-tabbar-item>
-    <van-tabbar-item replace to="/publish" icon="add-circle-o" name="/publish">发布行程</van-tabbar-item>
-    <van-tabbar-item replace to="/user" icon="manager-o" name="/user">个人中心</van-tabbar-item>
+  <van-tabbar route safe-area-inset-bottom active-color="#ff6600">
+    <van-tabbar-item replace to="/" icon="wap-home-o">首页大厅</van-tabbar-item>
+    <van-tabbar-item replace to="/publish" icon="add-circle-o">发布行程</van-tabbar-item>
+    <van-tabbar-item replace to="/user" icon="manager-o">个人中心</van-tabbar-item>
   </van-tabbar>
 </template>
 
 <style scoped>
-/* 增加顶部阴影，视觉上更清晰凸出 */
 .van-tabbar {
     box-shadow: 0 -2px 10px rgba(0,0,0,0.05);
 }
