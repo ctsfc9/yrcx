bfd4079cdd3d62c22f6798f32a3dc5f0f8dcde56
ctsfc9 <112675427@qq.com>
2026-05-20 09:27:50 +0800

    Update Me.vue

--- src/views/Me.vue
+++ src/views/Me.vue
@@ -2,9 +2,9 @@
   <div style="min-height: 100vh; background: #f7f8fa; padding-bottom: 80px;">
     <van-nav-bar title="个人中心" />
 
-    <div style="background: #fff; padding: 20px; display: flex; align-items: center;">
+    <div style="background: #fff; padding: 20px; display: flex; align-items: center; border-bottom: 1px solid #ebedf0;">
       <div v-if="user.id" style="display: flex; align-items: center;">
-        <img :src="user.avatar" style="width: 60px; height: 60px; border-radius: 50%;" />
+        <van-image round width="60" height="60" :src="user.avatar" />
         <div style="margin-left: 15px;">
           <div style="font-weight: bold; font-size: 16px;">{{ user.nickname }}</div>
           <div style="color: #999; font-size: 12px;">ID: {{ user.id }}</div>
@@ -16,34 +16,37 @@
     </div>
 
     <van-cell-group style="margin-top: 20px;">
-      <van-cell title="我的行程" is-link @click="showRides = true" />
-      <van-cell title="退出登录" style="color: red; text-align: center;" @click="logout" />
+      <van-cell title="我的行程" is-link to="/me/rides" />
+      <van-cell title="退出登录" style="color: #ee0a24; text-align: center;" @click="logout" />
     </van-cell-group>
 
-    <TabBar />
+    <Tabbar v-model="active" route>
+      <TabbarItem replace to="/" icon="home-o">首页</TabbarItem>
+      <TabbarItem replace to="/publish" icon="plus">发布</TabbarItem>
+      <TabbarItem replace to="/me" icon="user-o">我的</TabbarItem>
+    </Tabbar>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-import TabBar from '../components/TabBar.vue';
+import { Tabbar, TabbarItem } from 'vant';
 
+const active = ref(2); // 选中“我的”
 const user = ref({ id: '', nickname: '', avatar: '' });
-const router = useRouter();
 
 onMounted(() => {
   const profile = localStorage.getItem('user_profile');
   if (profile) {
     user.value = JSON.parse(profile);
   } else {
-    // 强制自动触发授权
+    // 初次打开自动跳转授权
     goToAuth();
   }
 });
 
 const goToAuth = () => {
-  const appId = 'wx90223bd25485040a'; // 保持您的AppID
+  const appId = 'wx90223bd25485040a';
   window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${encodeURIComponent(window.location.origin + '/me')}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`;
 };
 
