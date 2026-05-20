6b0a77652f04260cee49648be144dc3b7d30c95e
ctsfc9 <112675427@qq.com>
2026-05-20 08:42:42 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,21 @@ import router from './router'
 
 // 引入 Vant 全局样式
 import 'vant/lib/index.css'
-import { Button, NavBar, Form, Field, RadioGroup, Radio, Popup, Picker, Stepper, Toast, Dialog, Search, Icon, Tab, Tabs } from 'vant'
+// 补全所有用到的 Vant 组件
+import { 
+  Button, NavBar, Form, Field, RadioGroup, Radio, Popup, Picker, 
+  Stepper, Toast, Dialog, Search, Icon, Tab, Tabs,
+  List, Swipe, SwipeItem, NoticeBar, Tabbar, TabbarItem, Image as VanImage, CellGroup 
+} from 'vant'
 
 const app = createApp(App)
 
 // 注册所需组件
-const components = [Button, NavBar, Form, Field, RadioGroup, Radio, Popup, Picker, Stepper, Toast, Dialog, Search, Icon, Tab, Tabs]
+const components = [
+  Button, NavBar, Form, Field, RadioGroup, Radio, Popup, Picker, 
+  Stepper, Toast, Dialog, Search, Icon, Tab, Tabs,
+  List, Swipe, SwipeItem, NoticeBar, Tabbar, TabbarItem, VanImage, CellGroup
+]
 components.forEach(cmp => app.use(cmp))
 
 app.use(createPinia())
